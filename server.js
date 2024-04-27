45e5e513be6e7134948600af77a6abf5a1c138b9
sirri1222 <hyeonji159753@gmail.com>
2024-04-27 16:05:14 +0900

    Fix: 서버에 resolver 추가

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import { ApolloServer, gql } from "apollo-server";
+const tweets =[{
+    id:"1",
+    text:"hello1",
+
+},{
+    id:"2",
+    text:"hello2",
+
+},]
+
 
 
 const typeDefs = gql`
-//느낌표가 없으면 nullable field
+//느낌표가 없으면 nullable field require인지 말해주고있음
 type User{
     id:ID!
     username:String!
@@ -14,6 +24,7 @@ type User{
 //  타입 쿼리안에 넣으면 user 가 request 할수있는것들이 됨
 //db에 아무런 영향도 끼치지 않음.get request 같은거
 type Query {
+    // alltweets 라는 필드가 있다 
     allTweets:[Tweet!]!
     tweet(id:ID!):Tweet
     // user 가 data를 보내고 그게 backend 를 mutate 한다면 여기타입에 넣고 
@@ -25,11 +36,31 @@ type Mutation{
         postTweet(text:String!, userId: ID!): Tweet!
     deleteTweet(id :ID!): Boolean!
     }
-}`
+`;
+
+// apollo server 가 resolver 안에 function 을 부를 때 
+// function 에 arguments를 주는데 root argument 랑 걍 argument 가있음
+
+const resolvers = {
+    Query:{
+        allTweets(){
+            console.log("im called")
+            return tweets
+        },
+        // root 는 모르겟고 arguments 들은 쿼리나 mutation 에서 user가 보낸 arguments들이 될거
+        tweet(root, {id}){
+            console.log(arguments)
+            return tweets.find((tweet)=> id === tweet.id)
+        },
+        ping(){
+            console.log("im called")
+            return "pong"
+        },
+    }
+}
 
 
-
-const server = new ApolloServer({typeDefs})
+const server = new ApolloServer({typeDefs, resolvers})
  server.listen().then(({url})=>{
     console.log(`running on ${url}`)
  })
